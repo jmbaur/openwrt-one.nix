1525dbec6978e8321ac21020343e07c7804f0e50
Jared Baur <jaredbaur@fastmail.com>
2025-11-09 22:36:34 -0800

    Upgrade to 6.18-rc5

--- pkgs/openwrtOneLinux/package.nix
+++ pkgs/openwrtOneLinux/package.nix
@@ -5,11 +5,11 @@
 }:
 
 linuxKernel.manualConfig {
-  version = "6.18.0-rc4";
-  modDirVersion = "6.18.0-rc4";
+  version = "6.18.0-rc5";
+  modDirVersion = "6.18.0-rc5";
   src = fetchurl {
-    url = "https://git.kernel.org/torvalds/t/linux-6.18-rc4.tar.gz";
-    hash = "sha256-DtR8sFwexWyzondmRXXaSbHZ7W/QK2dMwp/zfg+TsKE=";
+    url = "https://git.kernel.org/torvalds/t/linux-6.18-rc5.tar.gz";
+    hash = "sha256-gozH1MrFNBJPzFOxoX4Q+y6INB1xy7MU/R81URkWUqM=";
   };
   configfile = ./kernel.config;
   kernelPatches = lib.mapAttrsToList (name: _: {
