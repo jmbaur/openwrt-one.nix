12f77b36ded6399d824caba027474981791d839d
Jared Baur <jaredbaur@fastmail.com>
2025-12-09 09:17:48 -0800

    Update inputs

--- pkgs/openwrtOneLinux/package.nix
+++ pkgs/openwrtOneLinux/package.nix
@@ -1,16 +1,7 @@
-{
-  fetchurl,
-  linuxKernel,
-  lib,
-}:
+{ lib, linuxKernel }:
 
 linuxKernel.manualConfig {
-  version = "6.18.0-rc5";
-  modDirVersion = "6.18.0-rc5";
-  src = fetchurl {
-    url = "https://git.kernel.org/torvalds/t/linux-6.18-rc5.tar.gz";
-    hash = "sha256-gozH1MrFNBJPzFOxoX4Q+y6INB1xy7MU/R81URkWUqM=";
-  };
+  inherit (linuxKernel.kernels.linux_6_18) src version;
   configfile = ./kernel.config;
   kernelPatches = lib.mapAttrsToList (name: _: {
     inherit name;
